fc5c69bbdd4be04eec326c66b7abeb0c2bd228d7
artofhunny <artofhunny@gmail.com>
2025-11-21 01:39:49 +0530

    changed the api-base-url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-export const BASE_URL =
-  window.location.hostname === "localhost"
-    ? "http://localhost:2400"
-    : "http://192.168.1.7:2400";
+// export const BASE_URL =
+//   window.location.hostname === "localhost"
+//     ? "http://localhost:2400"
+//     : "http://192.168.1.7:2400";
+
+export const BASE_URL = "https://hireino-backend.onrender.com";
